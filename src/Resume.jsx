18ea5746f9d90d8661da85ed3cd678f28223d198
Fabien <fabien.pinna@gmail.com>
2023-10-31 16:15:20 +0100

    App created with vite, ready to rock the world

---
+++ src/Resume.jsx
@@ -0,0 +1,14 @@
+import './Resume.scss'
+
+const Resume = () => {
+    return (
+        <div className="background">
+            <div className="layout-card">
+                <h1>Resume</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Resume

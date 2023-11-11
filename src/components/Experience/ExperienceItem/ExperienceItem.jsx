815638198ba2eb4fc470ecb4eb8c6141863797bb
Fabien <fabien.pinna@gmail.com>
2023-11-11 15:54:47 +0100

    Experience component almost done

--- src/components/Experience/ExperienceItem/ExperienceItem.jsx
+++ src/components/Experience/ExperienceItem/ExperienceItem.jsx
@@ -1,8 +1,20 @@
 import './ExperienceItem.scss'
 
-const ExperienceItem = () => {
+const ExperienceItem = ({ experienceTitle, experienceSubtitle, listItems }) => {
     return (
-        <div>ExperienceItem</div>
+        <div className='experience-item'>
+            <div className="experience-title">
+                <h4>{experienceTitle}</h4>
+                <h4>{experienceSubtitle}</h4>
+            </div>
+            <div className="experience-list">
+                <ul>
+                    {listItems.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
 

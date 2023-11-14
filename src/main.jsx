27b51299196400c8b3e7b620bec528e24a28680a
Fabien <fabien.pinna@gmail.com>
2023-11-14 16:15:37 +0100

    Threejs installed and work on 3D render started

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Resume from './Resume.jsx'
+import { Canvas } from '@react-three/fiber'
+import App from './App.jsx'
+import './style.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Resume />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Canvas
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 2000,
+      position: [-3, 1.5, 4]
+    }}
+  >
+    <App />
+  </Canvas>
 )

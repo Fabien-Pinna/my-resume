5ca4b7b32bade084adf9923f4599a9e3b4768f72
Fabien <fabien.pinna@gmail.com>
2023-11-20 16:16:09 +0100

    Work in progress on zoom animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
+import { useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 import { useGLTF, Environment, Float, PresentationControls, OrbitControls, ContactShadows, Html } from '@react-three/drei';
 import Resume from './components/Resume';
 
-export default function App() {
-
+const App = () => {
     // Load the model
     const resumeCard = useGLTF('/models/Laptop.glb');
 
@@ -13,48 +15,50 @@ export default function App() {
             <color args={['#241a1a']} attach={'background'} />
             <ambientLight intensity={0.3} />
 
-            <OrbitControls enableZoom={true} enablePan={false} enableRotate={false} maxZoom={0.8} minZoom={-1} />
 
-            {/* Controls that move the object according to user drag and drop */}
+            <OrbitControls zoom={true} enableRotate={false} />
+
+            {/* Controls that move the object  */}
             <PresentationControls
                 global
+                touchAction='none'
                 rotation={[0.13, 0.1, 0]}
                 polar={[-0.4, 0.2]}
                 azimuth={[-1, 0.08]}
                 config={{ mass: 4, tension: 400 }}
-                snap={{ mass: 4, tension: 400 }}
-                zoom={1}
+            // snap={{ mass: 4, tension: 400 }}
             >
                 {/* Computer with float animation */}
-                <Float rotationIntensity={0.4} >
-                    <rectAreaLight
-                        width={2.5}
-                        height={1.65}
-                        intensity={65}
-                        color={'#fff'}
-                        rotation={[0.1, Math.PI, 0]}
-                        position={[0, 0.55, -1.15]}
-                    />
+                <rectAreaLight
+                    width={2.5}
+                    height={1.65}
+                    intensity={65}
+                    color={'#fff'}
+                    rotation={[0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                />
 
-                    {/* Model */}
-                    <primitive
-                        object={resumeCard.scene}
-                        position-y={-0.5}
+                {/* Model */}
+                <primitive
+                    object={resumeCard.scene}
+                    position-y={-0.5}
+                >
+                    {/* Website displayed on screen */}
+                    <Html
+                        transform
+                        wrapperClass='htmlScreen'
+                        distanceFactor={1.05}
+                        position={[0.02, 1, -1]}
+                        style={{ width: 'auto', height: '90%' }} // Adjust the size as needed
+
                     >
-                        {/* Website displayed on screen */}
-                        <Html
-                            transform
-                            wrapperClass='htmlScreen'
-                            distanceFactor={1.05}
-                            position={[0.02, 1, -1]}
-                            style={{ width: 'auto', height: '90%' }} // Adjust the size as needed
-
-                        >
-                            <Resume />
-                        </Html>
-                    </primitive>
-                </Float>
-            </PresentationControls>
+                        <Resume />
+                    </Html>
+                </primitive>
+                <Html
+                    style={{ color: '#fff', fontSize: '5rem', position: 'absolute', top: '30%', left: '90%', transform: 'translate(-50%, -50%)' }}
+                >My Resume</Html>
+            </PresentationControls >
 
             <ContactShadows
                 position-y={-1.4}
@@ -65,3 +69,5 @@ export default function App() {
         </>
     );
 }
+
+export default App;

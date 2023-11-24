8c33602d47b65fbd90087bcaefa7973cd4be45b2
Fabien <fabien.pinna@gmail.com>
2023-11-24 23:16:41 +0100

    Good work session, found a good design

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls, ContactShadows, Html } from '@react-three/drei';
-import { ResumeCard } from './components/ResumeCard';
+import { OrbitControls, ContactShadows, Html, RoundedBox } from '@react-three/drei';
 import Resume from './components/Resume'
 
 const App = () => {
-
-
-
     return (
         <>
             <OrbitControls enableZoom={true} />
 
-            <ambientLight intensity={0.5} />
+            <color args={['#241a1a']} attach={'background'} />
+            <ambientLight intensity={0.3} />
 
-
-
-            <ResumeCard
-                position={[1, -3, -4]}
-                rotation={[0, -1.5, 0]}
-                scale={0.9}
-            />
-
+            <RoundedBox
+                args={[5.5, 4.1, 0]}
+                position={[0, 0, 0]}
+                rotation={[0, 0.5, 0]}
+                radius={0.2}
+            >
+                <Html
+                    transform
+                    wrapperClass='my-resume'
+                    distanceFactor={2}
+                    position={[0, 0, 0.2]}
+                >
+                    <Resume transparent />
+                </Html>
+            </RoundedBox>
             <ContactShadows
-                position-y={0}
-                opacity={1}
-                scale={5}
+                position-y={-3}
+                opacity={0.8}
+                scale={6}
                 blur={2.4}
             />
         </>

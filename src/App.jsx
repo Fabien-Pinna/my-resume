17e3dc713f42265add1a43806eea5e78bfef5ad3
Fabien <fabien.pinna@gmail.com>
2023-11-27 15:49:22 +0100

    Texture added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
 /* eslint-disable react/no-unknown-property */
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
-import { OrbitControls, ContactShadows, Html, RoundedBox, useTexture } from '@react-three/drei';
+import { OrbitControls, ContactShadows, Html, RoundedBox, useTexture, Environment } from '@react-three/drei';
 import Resume from './components/Resume'
 
 const App = () => {
     const boxRef = useRef();
     const htmlRef = useRef();
     const [isVisible, setIsVisible] = useState(true);
-    const colorTexture = useTexture('../public/textures/gold_1_baseColor.jpeg');
-    const ambientOcclusionTexture = useTexture('../public/textures/gold_1_ambientOcclusion.jpeg');
-    const heightTexture = useTexture('../public/textures/gold_1_height.jpeg');
 
-    // texture.repeat.set(1, 1);
-    // texture.offset.set(0, 0);
-    // texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    // Textures
+    const ambientOcclusionTexture = useTexture('/textures/Metal_006_ambientOcclusion.jpg');
+    const colorTexture = useTexture('/textures/Metal_006_basecolor.jpg');
+    const heightTexture = useTexture('/textures/Metal_006_height.png');
+    const metalnessTexture = useTexture('/textures/Metal_006_metallic.jpg');
+    const normalTexture = useTexture('/textures/Metal_006_normal.jpg');
+    const roughnessTexture = useTexture('/textures/Metal_006_roughness.jpg');
+
+    colorTexture.repeat.set(1, 1);
+    colorTexture.offset.set(0, 0);
+    colorTexture.wrapS = colorTexture.wrapT = THREE.RepeatWrapping;
+
+    ambientOcclusionTexture.repeat.set(1, 1);
+    ambientOcclusionTexture.offset.set(0, 0);
+    ambientOcclusionTexture.wrapS = ambientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+
+    heightTexture.repeat.set(1, 1);
+    heightTexture.offset.set(0, 0);
+    heightTexture.wrapS = heightTexture.wrapT = THREE.RepeatWrapping;
+
+    metalnessTexture.repeat.set(1, 1);
+    metalnessTexture.offset.set(0, 0);
+    metalnessTexture.wrapS = metalnessTexture.wrapT = THREE.RepeatWrapping;
+
+    normalTexture.repeat.set(1, 1);
+    normalTexture.offset.set(0, 0);
+    normalTexture.wrapS = normalTexture.wrapT = THREE.RepeatWrapping;
+
+    roughnessTexture.repeat.set(1, 1);
+    roughnessTexture.offset.set(0, 0);
+    roughnessTexture.wrapS = roughnessTexture.wrapT = THREE.RepeatWrapping;
 
 
     useFrame(({ camera }) => {
@@ -32,9 +57,13 @@ const App = () => {
 
     return (
         <>
-            <OrbitControls enableZoom={true} />
-            <color args={['#241a1a']} attach={'background'} />
-            <ambientLight intensity={0.3} />
+            <Environment preset='forest' />
+
+            <OrbitControls enableZoom={false} enablePan={false} />
+            <color args={['#0c001c']} attach={'background'} />
+
+
+
 
             <RoundedBox
                 ref={boxRef}
@@ -43,7 +72,21 @@ const App = () => {
                 rotation={[0, 0.5, 0]}
                 radius={0.2}
             >
-                <meshStandardMaterial attach='material' map={colorTexture} />
+                <meshStandardMaterial
+                    attach='material'
+                    map={colorTexture}
+                    aoMap={ambientOcclusionTexture}
+                    normalMap={normalTexture}
+                    metalnessMap={metalnessTexture}
+                    roughnessMap={roughnessTexture}
+                    metalness={0.5}
+                    roughness={0.2}
+                    displacementScale={0.1}
+
+                    displacementMap={heightTexture}
+                />
+
+                {/* <meshToonMaterial /> */}
 
                 {isVisible && (
                     <Html
@@ -54,6 +97,7 @@ const App = () => {
                         position={[0, 0, 0.2]}
                     >
                         <Resume transparent />
+
                     </Html>
 
                 )}

c75e7de451acb5a04b361fda7d5ec2d09fa25892
Fabien <fabien.pinna@gmail.com>
2023-12-07 12:20:27 +0100

    Done

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -3,7 +3,7 @@ import './Picture.scss'
 const Picture = () => {
     return (
         <div className='picture'>
-            <img src="src/assets/picture/fabio-2.png" alt="" />
+            <img src="/picture/fabio-2.png" alt="" />
         </div>
     )
 }
